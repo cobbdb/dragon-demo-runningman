d91ac2d41f7cb14cbb0292ef1bddad2736bf5e68
cobbdb <cobbdb@gmail.com>
2015-01-01 23:43:38 -0800

    external url test

--- src/game.js
+++ src/game.js
@@ -1,8 +1,28 @@
 var Dragon = require('dragonjs'),
     Game = Dragon.Game,
-    mainScreen = require('./screens/main.js');
+    getJSON = require('get-json');
 
-Game.addScreens(mainScreen);
+Game.addScreens(
+    require('./screens/main.js')
+);
 Game.run({
     debug: false
 });
+
+/**
+ * Side test to prove that the outside world
+ * can be touched after porting to mobile.
+ * This means that external services can be used.
+ */
+getJSON(
+    'http://time.jsontest.com',
+    function (err, res) {
+        if (err) {
+            console.log('Could not contact jsontest.com');
+        } else {
+            console.log(
+                JSON.stringify(res, null, 3)
+            );
+        }
+    }
+);
